e9ccb94ab364c423b61a9343715d6b1147da38be
iamnadiaim <iamnadiaintan@gmail.com>
2025-08-04 14:52:17 +0700

    login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'username' => 'admin',
+            'password' => Hash::make('123456'),
+            'alamat' => 'Banyuwangi',
+            'no_telepon' => '081234567890',
+            'role' => 'admin'
         ]);
     }
 }

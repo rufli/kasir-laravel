23e241eeb070b4c3188802dcf91789f1912459f9
rufli <ruflifirdaus@gmail.com>
2025-08-04 14:27:58 +0700

    mcr

---
+++ app/Http/Controllers/KategoriPengeluaranController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KategoriPengeluaran;
+use Illuminate\Http\Request;
+
+class KategoriPengeluaranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KategoriPengeluaran $kategoriPengeluaran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KategoriPengeluaran $kategoriPengeluaran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KategoriPengeluaran $kategoriPengeluaran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KategoriPengeluaran $kategoriPengeluaran)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/KategoriProdukController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KategoriProduk;
+use Illuminate\Http\Request;
+
+class KategoriProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KategoriProduk $kategoriProduk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KategoriProduk $kategoriProduk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KategoriProduk $kategoriProduk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KategoriProduk $kategoriProduk)
+    {
+        //
+    }
+}

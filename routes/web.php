505ab60318e66b5bfef1639708a5c1435a1f1dea
iamnadiaim <iamnadiaintan@gmail.com>
2025-08-06 17:32:05 +0700

    up route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,9 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\ProdukController;
-<<<<<<< HEAD
 use App\Http\Controllers\SignupController;
-=======
 use App\Http\Controllers\PengeluaranController;
-use App\Http\Controllers\KategoriPengeluaranController; // ← tambahkan ini
->>>>>>> 47a39adb369e990d4d14e818c471f8cb573828e4
+use App\Http\Controllers\KategoriPengeluaranController;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
@@ -24,7 +21,6 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
 
     Route::resource('kategori_produk', KategoriProdukController::class);
     Route::resource('produk', ProdukController::class);
-<<<<<<< HEAD
 
     Route::prefix('pegawai')->name('pegawai.')->group(function () {
         Route::get('/', [SignupController::class, 'index'])->name('index');
@@ -34,9 +30,6 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
         Route::put('/{id}', [SignupController::class, 'update'])->name('update');
         Route::delete('/{id}', [SignupController::class, 'destroy'])->name('destroy');
     });
-=======
     Route::resource('pengeluaran', PengeluaranController::class);
-
-    Route::resource('kategori_pengeluaran', KategoriPengeluaranController::class); // ← tambahkan ini
->>>>>>> 47a39adb369e990d4d14e818c471f8cb573828e4
+    Route::resource('kategori_pengeluaran', KategoriPengeluaranController::class);
 });

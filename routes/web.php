ca428fd808531ab974d01f2dce0fadc59e01848f
iamnadiaim <iamnadiaintan@gmail.com>
2025-08-06 13:25:00 +0700

    web route

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\ProdukController;
 
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});*/
 
-// Rute untuk halaman Dashboard
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index_alt'); // Opsional: rute alternatif
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
+    
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index_alt');
 
-// Rute resource untuk KategoriProduk
-Route::resource('kategori_produk', KategoriProdukController::class);
-
-// Rute resource untuk Produk
-Route::resource('produk', ProdukController::class);
+    Route::resource('kategori_produk', KategoriProdukController::class);
+    Route::resource('produk', ProdukController::class);
+});

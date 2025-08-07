ac54f55e60cf56457cd8939ef548f96b6d6cc018
rendhi69 <kancil9603@gmail.com>
2025-08-07 10:20:03 +0700

    update pengeluaran

--- resources/views/pengeluaran/index.blade.php
+++ resources/views/pengeluaran/index.blade.php
@@ -1,54 +1,75 @@
 @extends('layouts.app')
 
-@section('title', 'Pengeluaran')
+@section('title', 'Daftar Pengeluaran')
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('css/pengeluaran.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pengeluaran.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 @endpush
 
 @section('content')
-<div class="pengeluaran-container">
-    <div class="pengeluaran-header">
-        <h5>Pengeluaran</h5>
-        <div class="pengeluaran-actions">
-            <form action="{{ route('pengeluaran.index') }}" method="GET" class="search-form">
-                <input type="text" name="search" placeholder="search" value="{{ request('search') }}">
-            </form>
-            <a href="{{ route('pengeluaran.create') }}" class="btn-tambah">Tambah +</a>
+    <div class="pengeluaran-container">
+        <div class="pengeluaran-header">
+            <h5>Daftar Pengeluaran</h5>
+            <div class="pengeluaran-actions">
+                <form action="{{ route('pengeluaran.index') }}" method="GET" class="search-form">
+                    <div class="search-input-wrapper">
+                        <input type="text" name="search" placeholder="Search..." value="{{ request('search') }}">
+                        <button type="submit" class="search-btn">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+                <a href="{{ route('pengeluaran.create') }}" class="btn-tambah">Tambah +</a>
+            </div>
+        </div>
+
+        <div class="pengeluaran-table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Kategori</th>
+                        <th>Catatan</th>
+                        <th>Jumlah</th>
+                        <th>Tanggal</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($pengeluaran as $item)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $item->nama }}</td>
+                            <td>{{ $item->kategori->nama ?? '-' }}</td>
+                            <td>{{ $item->catatan ?? '-' }}</td>
+                            <td>Rp. {{ number_format($item->jumlah, 0, ',', '.') }}</td>
+                            <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d/m/Y') }}</td>
+                            <td>
+                                <div class="btn-action-group">
+                                    <a href="{{ route('pengeluaran.edit', $item->id) }}" class="btn-edit" title="Edit">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+
+                                    <form action="{{ route('pengeluaran.destroy', $item->id) }}" method="POST"
+                                        onsubmit="return confirm('Hapus pengeluaran ini?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn-delete" title="Hapus">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="7" class="text-center">Belum ada data pengeluaran.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </div>
-
-    <div class="pengeluaran-table">
-        <table>
-            <thead>
-                <tr>
-                    <th>Nama</th>
-                    <th>Kategori</th>
-                    <th>Catatan</th>
-                    <th>Jumlah</th>
-                    <th>Tanggal</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($pengeluaran as $item)
-                    <tr>
-                        <td>{{ $item->nama }}</td>
-                        <td>{{ $item->kategori->nama ?? '-' }}</td>
-                        <td>{{ $item->catatan ?? '-' }}</td>
-                        <td>Rp. {{ number_format($item->jumlah, 0, ',', '.') }}</td>
-                        <td>{{ \Carbon\Carbon::parse($item->tanggal)->format('d/m/Y') }}</td>
-                        <td>
-                            <a href="{{ route('pengeluaran.edit', $item->id) }}" class="btn-edit">Edit</a>
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="6">Belum ada data pengeluaran.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
-</div>
 @endsection

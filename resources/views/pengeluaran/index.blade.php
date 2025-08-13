07ff936b6411827d91662eb3c3b1794518a7f2b3
iamnadiaim <iamnadiaintan@gmail.com>
2025-08-13 15:01:18 +0700

    pengeluaran

--- resources/views/pengeluaran/index.blade.php
+++ resources/views/pengeluaran/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Daftar Pengeluaran')
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/pengeluaran.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/pengeluaran/index.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 @endpush
 

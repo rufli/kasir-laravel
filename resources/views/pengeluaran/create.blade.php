45f883b7a3dba54b940a56edf5f4969b076a57d6
rendhi69 <kancil9603@gmail.com>
2025-09-03 09:55:46 +0700

    up alert eror pengeluaran

--- resources/views/pengeluaran/create.blade.php
+++ resources/views/pengeluaran/create.blade.php
@@ -9,34 +9,33 @@
 @section('content')
     <div class="container">
         <h2>Tambah Pengeluaran</h2>
-
-
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul class="mb-0">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         <form action="{{ route('pengeluaran.store') }}" method="POST">
             @csrf
             <div class="row">
                 <!-- Tanggal -->
                 <div class="col-md-6 mb-3">
                     <label class="form-label" for="tanggal">Tanggal</label>
-                    <input type="date" name="tanggal" id="tanggal" class="form-control" value="{{ old('tanggal') }}"
-                        required>
+                    <input type="date"
+                           name="tanggal"
+                           id="tanggal"
+                           class="form-control @error('tanggal') is-invalid @enderror"
+                           value="{{ old('tanggal') }}">
+                    @error('tanggal')
+                   {{--<div class="error-message">{{ $message }}</div>--}}
+                    @enderror
                 </div>
 
                 <!-- Nama -->
                 <div class="col-md-6 mb-3">
                     <label class="form-label" for="nama">Nama</label>
-                    <input type="text" name="nama" id="nama" class="form-control" value="{{ old('nama') }}"
-                        required>
+                    <input type="text"
+                           name="nama"
+                           id="nama"
+                           class="form-control @error('nama') is-invalid @enderror"
+                           value="{{ old('nama') }}">
+                    @error('nama')
+                   {{--<div class="error-message">{{ $message }}</div>--}}
+                    @enderror
                 </div>
 
                 <!-- Kategori -->
@@ -48,7 +47,9 @@
                         </a>
                     </small>
 
-                    <select name="kategori_pengeluaran_id" id="kategori_pengeluaran_id" class="form-select custom-select">
+                    <select name="kategori_pengeluaran_id"
+                            id="kategori_pengeluaran_id"
+                            class="form-select custom-select @error('kategori_pengeluaran_id') is-invalid @enderror">
                         <option value="">-- Pilih Kategori --</option>
                         @foreach ($kategori as $item)
                             <option value="{{ $item->id }}"
@@ -57,19 +58,34 @@
                             </option>
                         @endforeach
                     </select>
+                    @error('kategori_pengeluaran_id')
+                    {{--<div class="error-message">{{ $message }}</div>--}}
+                    @enderror
                 </div>
 
                 <!-- Jumlah -->
                 <div class="col-md-6 mb-3">
                     <label class="form-label" for="jumlah">Jumlah</label>
-                    <input type="number" name="jumlah" id="jumlah" class="form-control" value="{{ old('jumlah') }}"
-                        required>
+                    <input type="number"
+                           name="jumlah"
+                           id="jumlah"
+                           class="form-control @error('jumlah') is-invalid @enderror"
+                           value="{{ old('jumlah') }}">
+                    @error('jumlah')
+                    {{--<div class="error-message">{{ $message }}</div>--}}
+                    @enderror
                 </div>
 
                 <!-- Catatan -->
                 <div class="col-md-6 mb-3">
                     <label class="form-label" for="catatan">Catatan</label>
-                    <textarea name="catatan" id="catatan" class="form-control" rows="3">{{ old('catatan') }}</textarea>
+                    <textarea name="catatan"
+                              id="catatan"
+                              class="form-control @error('catatan') is-invalid @enderror"
+                              rows="3">{{ old('catatan') }}</textarea>
+                    @error('catatan')
+                    {{--<div class="error-message">{{ $message }}</div>--}}
+                    @enderror
                 </div>
             </div>
 
